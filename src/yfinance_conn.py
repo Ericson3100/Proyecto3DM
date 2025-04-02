fb65439397ef3a9f45fcd457ca23121c8cad0c1f
Pablo Herrera <pablouski7@hotmail.com>
2025-04-02 15:55:29 -0500

    Update requirements and enhance EDA notebook with new visualizations and data analysis functions. Modify yfinance connection to retrieve maximum historical data at daily intervals.

--- src/yfinance_conn.py
+++ src/yfinance_conn.py
@@ -5,16 +5,20 @@ ticker_symbol = "^GSPC"
 
 historical_data = yf.download(
     tickers=ticker_symbol,
-    period="1y",
-    interval="1h",
+    period="max",
+    interval="1d",
     auto_adjust=True,
     prepost=False,
     threads=True
 )
 
-# Mostrar las primeras y últimas filas de los datos descargados
-print("Primeras filas de los datos históricos del S&P 500:")
-print(historical_data.head())
+# # Mostrar las primeras y últimas filas de los datos descargados
+# print("Primeras filas de los datos históricos del S&P 500:")
+# print(historical_data.head())
 
-print("\nÚltimas filas de los datos históricos del S&P 500:")
-print(historical_data.tail())
+# print("\nÚltimas filas de los datos históricos del S&P 500:")
+# print(historical_data.tail())
+
+print(historical_data.info())
+print(historical_data.describe())
+print(historical_data.isnull().sum())
