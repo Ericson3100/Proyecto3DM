b52e9762e3c875042537c053105b028ead320072
Pablo Herrera <pablouski7@hotmail.com>
2025-04-02 15:20:20 -0500

    Yfianance explore script

---
+++ src/yfinance_conn.py
@@ -0,0 +1,20 @@
+import yfinance as yf
+
+# Definir el símbolo del ticker para el S&P 500
+ticker_symbol = "^GSPC"  # Símbolo del S&P 500
+
+historical_data = yf.download(
+    tickers=ticker_symbol,
+    period="1y",
+    interval="1d",
+    auto_adjust=True,
+    prepost=False,
+    threads=True
+)
+
+# Mostrar las primeras y últimas filas de los datos descargados
+print("Primeras filas de los datos históricos del S&P 500:")
+print(historical_data.head())
+
+print("\nÚltimas filas de los datos históricos del S&P 500:")
+print(historical_data.tail())
